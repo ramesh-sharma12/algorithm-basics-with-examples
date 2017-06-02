f133e4f982b154efaec792c1910f42505fd706e2
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2017-06-02 14:02:34 +0530

    sorting updated

---
+++ sorting.js
@@ -0,0 +1,148 @@
+var BubbleSort = function (list) {
+    var list = [
+        12,
+        23,
+        45,
+        4,
+        5,
+        8,
+        15,
+        33
+    ];
+
+    for (var i = 0; i < list.length; i++) {
+        for (var j = i + 1; j < list.length; j++) {
+            if (list[i] > list[j]) {
+                var temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+                document.write('<br />swaping........(' + list[j] + ',' + list[i] + ')');
+                document.write('&nbsp;&nbsp;&nbsp;&nbsp;........' + list.join(' , '));
+            }
+        }
+        document.write('<br />');
+    }
+}
+
+BubbleSort();
+//BubbleSort2();
+
+var selectionSort = function () {
+    var list = [
+        12,
+        2,
+        45,
+        4,
+        77,
+        8,
+        15,
+        23
+    ];
+    document.write('<br /><br /><b>selectionSort steps........' + list.join(' , ') + '</b><br />');
+
+    for (var i = 0; i < list.length; i++) {
+        var min = i;
+        for (var j = i + 1; j < list.length; j++) {
+            if (list[j] < list[i]) { //23 < 12, 45<12, 4<12
+                min = j; // 3
+                var temp = list[i]; //  temp = 12
+                list[i] = list[min]; // list = 4
+                list[min] = temp; // list[min] = 12
+                document.write('<br />swaping........(' + list[min] + ',' + list[i] + ')');
+                document.write('&nbsp;&nbsp;&nbsp;&nbsp;........' + list.join(' , '));
+            }
+        }
+        document.write('<br />');
+    }
+}
+selectionSort();
+
+var insertionSort = function () {
+    var list = [
+        12,
+        2,
+        45,
+        4,
+        77,
+        8,
+        15,
+        23
+    ];
+    document.write('<b>insertionSort steps........' + list.join(' , ') + '</b>');
+
+    for (var i = 1; i < list.length; i++) {
+        var index = list[i];
+        var j = i;
+        while (j > 0 && list[j - 1] > index) {
+            list[j] = list[j - 1];
+
+            document.write('<br />swaping........(' + list[j] + ',' + index + ')');
+            document.write('&nbsp;&nbsp;&nbsp;&nbsp;........' + list.join(' , '));
+
+            j--;
+        }
+        list[j] = index;
+
+        document.write('<br />');
+    }
+}
+
+insertionSort();
+
+function mergeSort(arr) {
+    if (arr.length < 2) 
+        return arr;
+    
+    var middle = parseInt(arr.length / 2);
+    var left = arr.slice(0, middle);
+    var right = arr.slice(middle, arr.length);
+
+   document.write('<br /><br />' + left.join(', ') + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + right.join(','));
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    var result = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+
+        //document.write('&nbsp;&nbsp;&nbsp;' + result.join(', '));
+    }
+   
+
+    while (left.length) {
+        result.push(left.shift());
+        //document.write('' + left.join(', '));
+    }
+ 
+    while (right.length) {
+        result.push(right.shift());
+        //document.write('&nbsp;' + right.join(', '));
+    }  
+
+    document.write('||' + result.join(', '));
+
+    return result;
+}
+
+var arr = [
+    34,
+    203,
+    3,
+    746,
+    200,
+    984,
+    198,
+    764,
+    9
+];
+
+document.write('<br / ><b>merge sort........<br />' + arr.join(', ') + '</b>');
+
+document.write('<br / ><b>sorted........' + mergeSort(arr).join(' , ') + '</b>');
